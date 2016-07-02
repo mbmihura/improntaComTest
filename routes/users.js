9490511c5f606f73c10a8c9c8ce79664d3f2c74f
Martin Mihura <mbmihura@monits.com>
2016-07-02 15:21:41 -0300

    Add mongo and endpoints.

--- routes/users.js
+++ routes/users.js
@@ -10,199 +10,81 @@ var Db = require('mongodb').Db,
   GridStore = require('mongodb').GridStore,
   Code = require('mongodb').Code;
 
-var db = new Db('geospatial', new Server("13.68.16.52", 20000,
-  { auto_reconnect: true, poolSize: 4 }), { w: 0, native_parser: false });
+const DEFAULT_ENDPOINT_RETURN_LIMIT = 25;
 
-console.log("config..") 
+var url = 'mongodb://dblandit:pilar@13.68.16.52:20000/geospatial?authMechanism=DEFAULT&authSource=admin';
+MongoClient.connect(url, function (err, db) {
+  console.log("Connected correctly to server");
 
-// Establish connection to db
-db.open(function (err, db) {
-  // Authenticate
-  db.authenticate('dblandit', 'pilar', function (err, result) {
-    console.log("mongo ready") 
-    console.log(err)
-     console.log(result) 
-    /* GET users listing. */
-    router.get('/getCell', function (req, res, next) {
-      if (req.query.radio && req.query.mcc && req.query.net && req.query.area && req.query.cell) {
-        console.log("go to mongo")
-        db.collection('mycollection').find().toArray(function (err, items) {
-          res.json(items);
-        });
-        /*res.json({
-          "radio": req.query.radio,
-          "mcc": req.query.mcc,
-          "net": req.query.net,
-          "area": req.query.area,
-          "cell": req.query.cell,
-          "range": 33,
-          "samples": 3,
-          "changeable": 1,
-          "created": new Date("2011-03-07T08:33:12Z"),
-          "updated": new Date(),
-          "averageSignal": -94,
-          "geometry": {
-            "type": "Point",
-            "coordinates": [13.285081, 52.521622]
-          },
-          "OID": 3
-        });*/
-      } else {
-        console.log("this are your params")
-        res.json(req.query)
-        /*res.status(404);
-        var err = new Error('Not Found');
-        err.status = 404;
-        res.render('error', {
-          message: "Not Found",
-          error: err
-        });*/
-      }
-    });
+  router.get('/getCell', function (req, res, next) {
+    var params = {
+      radio: req.query.radio,
+      mcc: parseInt(req.query.mcc),
+      net: parseInt(req.query.net),
+      area: parseInt(req.query.area),
+      cell: parseInt(req.query.cell)
+    }
+    if (params.radio && !isNaN(params.mcc) && !isNaN(params.net) && !isNaN(params.area) && !isNaN(params.cell)) {
+      console.log('searching for cell with:')
+      console.log(params)
+      db.collection('FC_Cell_towers').findOne(params, function (err, item) {
+        if (item) {
+          res.json(item);
+        } else {
+          res.status(404);
+          var err = {
+            message: 'Cell not found for the given params',
+            code: 4041,
+            searchByParams: params,
+          }
+          res.json(err);
+        }
+      });
+    } else {
+      sendMissingParamsResponse(res, params);
+    }
+  });
 
-    router.get('/getCellsNearPoint', function (req, res, next) {
-      if (req.query.latitude && req.query.longitude) {
-        res.json([{
-          "radio": "UMTS",
-          "mcc": 262,
-          "net": 2,
-          "area": 801,
-          "cell": 211250,
-          "range": 33,
-          "samples": 3,
-          "changeable": 1,
-          "created": new Date("2010-08-23T13:19:34Z"),
-          "updated": new Date("2011-03-07T08:33:12Z"),
-          "averageSignal": -94,
-          "geometry": {
-            "type": "Point",
-            "coordinates": [
-              13.285081,
-              52.521622
-            ]
-          },
-          "OID": 3
-        }, {
-            "radio": "UMTS",
-            "mcc": 342,
-            "net": 4,
-            "area": 431,
-            "cell": 955251,
-            "range": 35,
-            "samples": 2,
-            "changeable": 1,
-            "created": new Date("2010-08-25T13:19:34Z"),
-            "updated": new Date("2011-04-31T18:33:12Z"),
-            "averageSignal": -50,
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                15.234580,
-                23.534566
-              ]
-            },
-            "OID": 3
-          }]);
-      } else {
-        res.json(req.query)
-        /*res.status(404);
-        var err = new Error('Not Found');
-        err.status = 404;
-        res.render('error', {
-          message: "Not Found",
-          error: err
-        });*/
-      }
-    });
+  router.get('/getCellsNearPoint', function (req, res, next) {
+    var params = {
+      latitude: parseFloat(req.query.latitude),
+      longitude: parseFloat(req.query.longitude),
+      maximumDistance: req.query.maximumDistance ? parseInt(req.query.maximumDistance) : 10000,
+      limit: req.query.limit ? parseInt(req.query.limit) : DEFAULT_ENDPOINT_RETURN_LIMIT,
+      skip: req.query.skip ? parseInt(req.query.skip) : 0
+    }
+    if (!isNaN(params.latitude) && !isNaN(params.longitude)) {
+      console.log('searching for cell near point:')
+      console.log(params)
+      db.collection('FC_Cell_towers').find(
+          { 'geometry':
+            { $near :
+              { $geometry:
+                { type: "Point",  coordinates: [ params.latitude, params.longitude ] },
+                  $maxDistance: params.maximumDistance
+              }
+            }
+          }
+          ).skip(params.skip).limit(params.limit).toArray(function(err, docs) {
+            console.log('found:')
+            console.log(docs)
+            res.json(docs);
+          });
+    } else {
+      sendMissingParamsResponse(res, params);
+    }
   });
 });
 
-/* GET users listing. */
-router.get('/getCell', function (req, res, next) {
-  if (req.query.radio && req.query.mcc && req.query.net && req.query.area && req.query.cell) {
-    res.json({
-      "radio": req.query.radio,
-      "mcc": req.query.mcc,
-      "net": req.query.net,
-      "area": req.query.area,
-      "cell": req.query.cell,
-      "range": 33,
-      "samples": 3,
-      "changeable": 1,
-      "created": new Date("2011-03-07T08:33:12Z"),
-      "updated": new Date(),
-      "averageSignal": -94,
-      "geometry": {
-        "type": "Point",
-        "coordinates": [13.285081, 52.521622]
-      },
-      "OID": 3
-    });
-  } else {
-    res.json(req.query)
-    /*res.status(404);
-    var err = new Error('Not Found');
-    err.status = 404;
-    res.render('error', {
-      message: "Not Found",
-      error: err
-    });*/
+function sendMissingParamsResponse(res, params) {
+  res.status(422);
+  var err = {
+    message: 'Missing or invalid param',
+    code: 4221,
+    extractedParams: params,
   }
-});
-
-router.get('/getCellsNearPoint', function (req, res, next) {
-  if (req.query.latitude && req.query.longitude) {
-    res.json([{
-      "radio": "UMTS",
-      "mcc": 262,
-      "net": 2,
-      "area": 801,
-      "cell": 211250,
-      "range": 33,
-      "samples": 3,
-      "changeable": 1,
-      "created": new Date("2010-08-23T13:19:34Z"),
-      "updated": new Date("2011-03-07T08:33:12Z"),
-      "averageSignal": -94,
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          13.285081,
-          52.521622
-        ]
-      },
-      "OID": 3
-    }, {
-        "radio": "UMTS",
-        "mcc": 342,
-        "net": 4,
-        "area": 431,
-        "cell": 955251,
-        "range": 35,
-        "samples": 2,
-        "changeable": 1,
-        "created": new Date("2010-08-25T13:19:34Z"),
-        "updated": new Date("2011-04-31T18:33:12Z"),
-        "averageSignal": -50,
-        "geometry": {
-          "type": "Point",
-          "coordinates": [
-            15.234580,
-            23.534566
-          ]
-        },
-        "OID": 3
-      }]);
-  } else {
-    res.json(req.query)
-    /*res.status(404);
-    var err = new Error('Not Found');
-    err.status = 404;
-    res.render('error', {
-      message: "Not Found",
-      error: err
-    });*/
-  }
-});
+  res.json(err);
+  console.log(err);
+}
 
 module.exports = router;
